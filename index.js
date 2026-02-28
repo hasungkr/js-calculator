53b0be8c7fd9739faac1b83f7c18b7942ed1c319
hasungnz <hasunglee132@gmail.com>
2026-02-28 18:17:31 +1300

    add js code functions for buttons.

---
+++ index.js
@@ -0,0 +1,21 @@
+// calculator program
+
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(input){
+    display.value = "";
+}
+
+function calculate(input){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+}
